c80b4a747994e8dac862933ff1be6ebc857810b2
inkiu <596970601@qq.com>
2020-04-23 19:06:54 +0800

    把flask官方教程的项目做完了

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -30,4 +30,9 @@ def create_app(test_config=None):
         return 'Hello, World!'
     from . import db
     db.init_app(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     return app
